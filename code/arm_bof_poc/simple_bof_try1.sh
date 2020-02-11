d0d67939951fde37ab30d64918aba78bc82a7d31
Kaiwan N Billimoria <kaiwan.billimoria@gmail.com>
2020-02-11 07:37:58 +0000

    simple_bof_try1.sh: updated..

--- code/arm_bof_poc/simple_bof_try1.sh
+++ code/arm_bof_poc/simple_bof_try1.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+name=$(basename "$0")
 PUT=arm_bof_vuln  # Program Under Test
 
 is_stack_guarded()
@@ -7,9 +8,24 @@ echo -n " $1 : Is Stack Guarded? "
 readelf -s ${1} |grep -q __stack_chk_guard && echo "Yes!" || echo "Nope"
 }
 
-echo "1. BOF on ${PUT}:"
-is_stack_guarded ${PUT}
+test1()
+{
+local re
+echo "1. Run BOF on ${PUT}? [y/N] "
+read re
+[ "${re}" != "y" -a "${re}" != "Y" ] && return
 perl -e 'print "A"x12 . "B"x4 . "C"x4' | ./${PUT}
+}
+
+
+## 'main'
+[ ! -f ${PUT} ] && {
+  echo "${name}: binary executable \"${PUT}\" missing.."
+  exit 1
+}
+
+is_stack_guarded ${PUT}
+test1
 
 echo
 PUT=arm_bof_vuln_stackprot
